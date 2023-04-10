7b7bfeb7dfb53e232bf091a647c917be470827fa
yoongi <lolhongy3@gmail.com>
2023-04-10 20:47:52 +1000

    add grid size and refresh system

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const container = document.getElementById("container");
+const boxSize = 512;
+const maxSize = 100;
+
+const container = document.getElementById("grid_container");
 let rows = document.getElementsByClassName("row");
-let cells = document.getElementsByClassName("cell");
+let cells = document.getElementsByClassName("cells");
+
+const setGridBtn = document.getElementById("grid_size");
+setGridBtn.addEventListener('click', setGridSize)
 
 function createGrid(gridSize){
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+
     // Create Rows
     for(i=0; i<gridSize; i++){
         let newRow = document.createElement("div");
@@ -15,13 +25,24 @@ function createGrid(gridSize){
     }
 
     var gridCells = document.querySelectorAll('.cells'); 
+    gridCells.forEach(cells => cells.style.height = boxSize/gridSize);
+    gridCells.forEach(cells => cells.style.width = boxSize/gridSize);
     gridCells.forEach(cells => cells.addEventListener('click', changeColor));
 }
 
 function changeColor(){
     this.style.backgroundColor = '#ff9999';
-    console.log("change");
 }
 
-createGrid(16);
+function setGridSize(){
+    let gridSize = prompt("Enter grid width", "16");
+
+    if(gridSize > maxSize){
+        alert("Enter an integer between 1 and 100", "16");
+        setGridSize();
+    }
+    if(gridSize <= maxSize){
+        createGrid(gridSize);
+    }
+}
 
