21df5149ac7205f59bf888e80f3766aefb9bf59c
yoongi <lolhongy3@gmail.com>
2023-04-13 15:03:30 +1000

    add general style

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const boxSize = 512;
 const maxSize = 100;
 
-const container = document.getElementById("grid_container");
+const container = document.querySelector(".grid-container");
 let rows = document.getElementsByClassName("row");
 let cells = document.getElementsByClassName("cells");
 
 const setGridBtn = document.getElementById("grid_size");
 setGridBtn.addEventListener('click', setGridSize)
+
+const defaultSize = 16;
+const defaultMode = 'mono'
+
+let currentMode = defaultMode;
+
+const rgbBtn = document.getElementById("rgb_toggle");
+const monoBtn = document.getElementById("mono_toggle");
+
+rgbBtn.onclick = () => toggleMode('rainbow');
+monoBtn.onclick = () => toggleMode('mono');
 
 function createGrid(gridSize){
     while (container.lastElementChild) {
@@ -25,24 +36,39 @@ function createGrid(gridSize){
     }
 
     var gridCells = document.querySelectorAll('.cells'); 
+    // Set cell size to fit container
     gridCells.forEach(cells => cells.style.height = boxSize/gridSize);
     gridCells.forEach(cells => cells.style.width = boxSize/gridSize);
-    gridCells.forEach(cells => cells.addEventListener('click', changeColor));
+    // Add OnClick for each cell 
+    gridCells.forEach(cells => cells.addEventListener('mouseover', changeColor));
+}
+
+function toggleMode(mode){
+    currentMode = mode;
+    console.log(currentMode);
 }
 
 function changeColor(){
-    this.style.backgroundColor = '#ff9999';
+    if(currentMode === 'rainbow'){
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        this.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+    if(currentMode === 'mono')
+        this.style.backgroundColor = '#000000';
 }
 
 function setGridSize(){
     let gridSize = prompt("Enter grid width", "16");
 
-    if(gridSize > maxSize){
+    if(gridSize > maxSize || gridSize < 1){
         alert("Enter an integer between 1 and 100", "16");
         setGridSize();
     }
-    if(gridSize <= maxSize){
+    if(gridSize <= maxSize && gridSize >= 1){
         createGrid(gridSize);
     }
 }
 
+createGrid(defaultSize);
